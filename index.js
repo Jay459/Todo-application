55a5e401f290a5347a074d82cca0075535bdcc5b
jay459 <anigounigoud@gmail.com>
2021-01-10 12:34:40 +0530

    changes made in index.js file

--- index.js
+++ index.js
@@ -1,21 +1,11 @@
 const express = require('express');
 const app = express();
+const db = require('./database/database')
 app.set('view engine', 'ejs');
+const routes = require('./routes/routes');
 const port = 3000
 
-app.get('/' , function(req, res){
-    res.render('home');
-});
-
-
-app.get('/add-item' , function(req, res){
-    res.render('add-item');
-});
-
-app.use(function(req, res){
-    res.render('error');
-});
-
+app.use(routes);
 
 app.listen(port, () => {
     console.log(`Server started on ${port}`);
